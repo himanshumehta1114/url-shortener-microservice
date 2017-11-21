b70ed00a53907a4b96d515b96bb62c4fe556f772
himanshu <himanshumehta1114@gmail.com>
2017-11-21 19:58:47 +0530

    created /new/:url ROUTE

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+var mongoose = require('mongoose');
+const port = process.env.PORT || 3000;
+const app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://root:1234@ds113746.mlab.com:13746/little-url');
+
+var urlDir = mongoose.model('urlDirectory', {
+    url : {
+      type : String,
+      required : true,
+      minlength : 1
+    },
+    shortLink : {
+      type : Number,
+      default : null
+    }
+});
+
+app.get('/', (req,res) => {
+  res.send('Welcome to home page');
+});
+
+app.get('/new/:url', (req,res) => {
+    var shortID = Math.floor(Math.random() * 10000);
+    console.log(shortID);
+
+    var shortUrl = new urlDir({
+      url : req.params.url,
+      shortLink : shortID
+    });
+
+    shortUrl.save().then((doc) => {
+      res.send(doc);
+    }, (e) => {
+      console.log(`Unable to save to db`);
+    })
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is up on port : ${port}`);
+});
